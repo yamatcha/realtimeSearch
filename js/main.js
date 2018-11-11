fb00633ff34216c005c07078a3d48d74a115b67b
yamatcha <sojusojuonetmrlarcbz@gmail.com>
2018-11-11 17:28:22 +0900

    This is first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: null,
+        keyword: '',
+        message: ''
+    },
+    watch: {
+
+    },
+    created: function(){
+        this.keyword = 'JavaScript'
+        this.getAnswer()
+    },
+    methods: {
+        getAnswer: function(){
+            if(this.keyword === ''){
+                this.items = null
+                return
+            }
+
+            this.message = 'Loading...'
+            var vm = this
+            var params ={ page:1 , per_page: 20, query: this.keyword }
+            axios.get('https://qiita.com/api/v2/items',{ params })
+            .then(function(response){
+                //console.log(response)
+                vm.items = response.data
+            }).catch(function(error){
+                vm.message = 'Error!'+ error
+            })
+            .finally(function(){
+                vm.message = ''
+            })
+        }
+    }
+})
